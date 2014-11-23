720480fad9c6729cb833ef328b3f84c5787581e9
Elliot Colp <thelastbanana@gmail.com>
2014-11-23 13:21:06 -0700

    Hid all panels except status

---
+++ javascript.js
@@ -0,0 +1,10 @@
+ function hidePanels(){
+   compassAccordion.style.display = "none";
+   compassLabel.style.display = "none";
+   
+   inventoryAccordion.style.display = "none";
+   inventoryLabel.style.display = "none";
+   
+   placesObjectsAccordion.style.display = "none";
+   placesObjectsLabel.style.display = "none";
+ }
